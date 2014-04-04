f0ef26648fa4bcbc20786e5e876b8740d5780d0a
James Pells <jimmy@jamespells.com>
2014-04-04 17:15:02 -0400

    start theming the website

--- settings.py
+++ settings.py
@@ -299,6 +299,8 @@ TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
 ################
 
 INSTALLED_APPS = (
+    "customtheme",
+
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
