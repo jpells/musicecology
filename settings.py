4cc1793162dbc7976338f75aa4761175f973deca
James Pells <jimmy@jamespells.com>
2014-04-04 09:41:02 -0400

    update a few settings

--- settings.py
+++ settings.py
@@ -156,13 +156,13 @@ USE_SOUTH = True
 # In the format (('Full Name', 'email@example.com'),
 #                ('Full Name', 'anotheremail@example.com'))
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    ('Webmaster', 'webmaster@musicecologyboston.com'),
 )
 MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".musicecologyboston.com",]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -171,7 +171,7 @@ ALLOWED_HOSTS = []
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = None
+TIME_ZONE = "America/New_York"
 
 # If you set this to True, Django will use timezone-aware datetimes.
 USE_TZ = True
