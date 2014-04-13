507fbeb07fbab6955adceb0ac7f8286698d651fa
James Pells <jimmy@jamespells.com>
2014-04-12 20:14:36 -0400

    fix magnific-popup js error when using django-compressor

--- settings.py
+++ settings.py
@@ -162,7 +162,10 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [".musicecologyboston.com",]
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '.musicecologyboston.com',
+]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
